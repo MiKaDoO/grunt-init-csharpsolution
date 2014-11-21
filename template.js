33ff86d0f984b9a8981c3ae182d2aae99fec548d
unknown <m.metesreau@FR-D-102.be-ad.loc>
2014-11-21 12:35:49 +0100

    Moving to Paket

--- template.js
+++ template.js
@@ -14,7 +14,7 @@ exports.description = 'Create a C# solution with main project + nunit test proje
 exports.notes = '';
 
 // Template-specific notes to be displayed after question prompts.
-exports.after = 'You should now install project dependencies with nuget restore'
+exports.after = 'You should now install project dependencies with paket'
 
 // Any existing file or directory matching this wildcard will cause a warning.
 exports.warnOn = '*';
